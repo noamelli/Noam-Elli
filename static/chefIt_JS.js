1b87b6ea200babd5ab00bac0e1d0adceba6e0607
noamelli <Noameli90@gmail.com>
2022-05-17 13:44:38 +0300

    updating

--- static/chefIt_JS.js
+++ static/chefIt_JS.js
@@ -1,16 +1,18 @@
-
-
-//pull the pathname from window location
-// const activePage = window.location.pathname;
-// console.log(window);
-// console.log(window.location);
-// console.log(activePage);
-
-/*create an arey of the links in nav, 
-compare each to pathname and mark the one that is active
-*/ 
-const navLinks = document.querySelectorAll("navigation").forEach(link => {    
-  if(link.href.includes(`${activePage}`)){
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+  if (link.href.includes(`${activePage}`)) {
     link.classList.add('active');
   }
 });
+var i = 0;
+var txt = ["Please choose the category you want"]; /* The text */
+var speed = 80;
+
+typeWriter = () => {
+  document.querySelector("#demo").innerHTML = txt[0].substring(0, i) + "<span>\u25ae </span>";
+  if (i++ != txt[0].length) {
+    setTimeout(typeWriter, speed);
+  }
+}
+window.addEventListener("load", typeWriter)
+
